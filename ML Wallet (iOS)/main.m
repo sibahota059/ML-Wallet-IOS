b8716625697c68c3f5ba0bd484cf2706af5df8f5
ml <mm20-3@mm20-3.local>
2014-02-17 13:33:47 +0800

    Initial Commit

---
+++ ML Wallet (iOS)/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ML Wallet (iOS)
+//
+//  Created by ml on 2/17/14.
+//  Copyright (c) 2014 ML Lhuillier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
