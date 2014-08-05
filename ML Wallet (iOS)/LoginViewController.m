17b60b845d45279e7a8bc41d98ea0f43db0bb675
haruld <mm20-12@mm20-12.local>
2014-08-05 17:47:12 +0800

    Revert 433877f..2fe5307
    This rolls back to commit 433877f5dc20d4062549a0681b3d6e1f3411baee.

--- ML Wallet (iOS)/LoginViewController.m
+++ ML Wallet (iOS)/LoginViewController.m
@@ -168,14 +168,14 @@
 }
 
 - (IBAction)btnRegister:(id)sender {
-    [UIAlertView myCostumeAlert:@"Todo" alertMessage:@"Under development" delegate:nil cancelButton:@"Ok" otherButtons:nil];
-    /*
+    
+    
     EnterCustomerID *enterCustomer = [[EnterCustomerID alloc] initWithNibName:@"EnterCustomerID" bundle:nil];
     
     
     [self.navigationController pushViewController:enterCustomer animated:YES];
     self.navigationController.navigationBarHidden = NO;
-*/
+
 
 }
 
@@ -442,7 +442,7 @@
     
     //if all OK... then procced
     NSString *deviceID = [[DeviceID alloc] NSGetDeviceID];
-    NSString *version =[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSString *version =[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
     
     
     NSString *post = [NSString stringWithFormat:@"{\"username\" : \"%@\",\"password\" : \"%@\",\"version\" : \"%@\",\"latitude\" : \"%f\",\"longitude\" : \"%f\",\"deviceid\" : \"%@\",\"location\" : \"%@\"}",
