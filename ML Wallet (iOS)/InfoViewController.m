566554b6ced041b3d442974c9ed45e59ca81db57
haruld <mm20-12@mm20-12.local>
2014-08-05 09:49:34 +0800

    Revert ff053fc..f48a697
    This rolls back to commit ff053fce5f088af37ffc7e0eb299b5048702b8f2.

--- ML Wallet (iOS)/InfoViewController.m
+++ ML Wallet (iOS)/InfoViewController.m
@@ -40,10 +40,15 @@
 {
     self.navigationController.navigationBarHidden = NO;
     self.title= @"Information";
-    UIBarButtonItem *btnHome = [[UIBarButtonItem alloc] initWithTitle:@"Back"
-                                                                style:UIBarButtonItemStyleBordered
+    UIBarButtonItem *btnHome = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back.png"]
+                                                                style:UIBarButtonItemStylePlain
                                                                target:self
                                                                action:@selector(gotoHome)];
+    
+//    UIBarButtonItem *btnHome = [[UIBarButtonItem alloc] initWithTitle:@"Back"
+//                                                                style:UIBarButtonItemStyleBordered
+//                                                               target:self
+//                                                               action:@selector(gotoHome)];
     
     self.navigationItem.leftBarButtonItem = btnHome;
     
