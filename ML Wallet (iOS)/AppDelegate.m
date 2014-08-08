fd7cb3895bdbdc494c09c37539694f32aee9b542
ybanezmarjune <ybanezmarjune@gmail.com>
2014-08-08 08:57:17 +0800

    Hide keyboard when going background :D

--- ML Wallet (iOS)/AppDelegate.m
+++ ML Wallet (iOS)/AppDelegate.m
@@ -87,6 +87,8 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    NSLog(@"GOing background");
+    [self.navigationController.view endEditing:YES];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
