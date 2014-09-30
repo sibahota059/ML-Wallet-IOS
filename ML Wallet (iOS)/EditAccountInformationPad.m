378679384aa426e4930716c1b037b04864311e9f
harrylingad2013 <mm20-18@mm20-18.local>
2014-09-30 08:51:07 +0800

    update_9/30/2014

--- ML Wallet (iOS)/EditAccountInformationPad.m
+++ ML Wallet (iOS)/EditAccountInformationPad.m
@@ -1247,7 +1247,10 @@ NSString *strImage1, *strImage2, *strImage3, *strImage4;
     {
         NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
         
-        if ([string rangeOfCharacterFromSet:set].location != NSNotFound)
+        if([string isEqualToString:@""] && range.length == 1)
+        {
+            return YES;
+        }else if ([string rangeOfCharacterFromSet:set].location != NSNotFound)
         {
             return YES;
         }
