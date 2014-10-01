5f7efecc76757851fe7f02d9dd312567ba39d349
ybanezmarjune <mm20@mm20-13.local>
2014-10-01 17:24:18 +0800

    update balance format

--- ML Wallet (iOS)/MLSendMoneyViewController.m
+++ ML Wallet (iOS)/MLSendMoneyViewController.m
@@ -171,8 +171,12 @@
     _receiverImage.image = [UIImage imageNamed:@"noImage.png"];
 
     //Get the total ammount of transaction and deduct to the balance
-    double getTotal = [_label_balance.text doubleValue] - [_totalValue.text doubleValue];
-    _label_balance.text = [NSString stringWithFormat:@"%0.2f", getTotal];
+    NSString *str = _label_balance.text;
+    NSString* cleanedString = [[str stringByReplacingOccurrencesOfString:@"," withString:@""]
+                               stringByTrimmingCharactersInSet: [NSCharacterSet symbolCharacterSet]];
+    double getTotal = [cleanedString doubleValue] - [_totalValue.text doubleValue];
+    //_label_balance.text = [NSString stringWithFormat:@"%0.2f", getTotal];
+    _label_balance.text = [NSString stringWithFormat:@"%@", [self convertDecimal:getTotal]];
     _totalValue.text = @"0.00";
     
     //Get the user balance
